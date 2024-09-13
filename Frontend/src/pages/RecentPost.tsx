982def5562862370040464000c70f96f9b32d097
ddddsssss4 <saraswatdevesh98@gmail.com>
2024-09-13 12:21:26 +0530

    resolving build errors

--- Frontend/src/pages/RecentPost.tsx
+++
@@ -1,5 +0,0 @@
-import React, { useState } from 'react';
-import { Appbar } from '../components/Appbar';
-import { BlogCard } from '../components/BlogCard';
-import { BlogSkeleton } from '../components/BlogSkeleton';
-import { useBlogs } from '../hooks';
